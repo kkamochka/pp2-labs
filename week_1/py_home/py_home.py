d72900986a6906d684a865dea8e66720efeeea63
kkamochka <k_rinatkyzy@kbtu.kz>
2024-01-26 20:49:09 +0600

    Python Weekochka 1 Yeeeey

--- week_1/py_home/py_home.py
+++ week_1/py_home/py_home.py
@@ -1,2 +1,8 @@
-# With our "Try it Yourself" editor, you can edit Python code and view the result.
+# With our "Try it Yourself" editor, 
+# you can edit Python code and view the result.
 print("Hello, World!")
+
+if 5>2:
+    print("YES")
+else:
+    print("NO")
---
+++ week_1/py_intro/py_intro.py
@@ -0,0 +1,8 @@
+# Comments in Python are written with a special character, which one?
+
+# This is a comment
+"""
+This is a comment
+written in 
+more than just one line
+"""
