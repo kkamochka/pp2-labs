9b5d676233d6c9b31a39629b745a2525e64ddbfc
kkamochka <k_rinatkyzy@kbtu.kz>
2024-02-06 15:28:32 +0600

    yey

--- week_2/py_sets/py_sets.py
+++ week_2/py_sets/py_sets.py
@@ -1,27 +1,27 @@
-Set
-Sets are used to store multiple items in a single variable.
+# Set
+# Sets are used to store multiple items in a single variable.
 
-Set is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Tuple, and Dictionary, all with different qualities and usage.
+# Set is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Tuple, and Dictionary, all with different qualities and usage.
 
-A set is a collection which is unordered, unchangeable*, and unindexed.
+# A set is a collection which is unordered, unchangeable*, and unindexed.
 
-* Note: Set items are unchangeable, but you can remove items and add new items.
+# * Note: Set items are unchangeable, but you can remove items and add new items.
 
-Sets are written with curly brackets.
+# Sets are written with curly brackets.
 
 
-Create a Set:
+# Create a Set:
 
-thisset = {"apple", "banana", "cherry"}
-print(thisset)
+# thisset = {"apple", "banana", "cherry"}
+# print(thisset)
 
-Set Items
-Set items are unordered, unchangeable, and do not allow duplicate values.
+# Set Items
+# Set items are unordered, unchangeable, and do not allow duplicate values.
 
-Unordered
-Unordered means that the items in a set do not have a defined order.
+# Unordered
+# Unordered means that the items in a set do not have a defined order.
 
-# Set items can appear in a different order every time you use them, and cannot be referred to by index or key.
+# # Set items can appear in a different order every time you use them, and cannot be referred to by index or key.
 
 # Unchangeable
 # Set items are unchangeable, meaning that we cannot change the items after the set has been created.
