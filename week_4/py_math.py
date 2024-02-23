51d1729dd84d7cacc630bb7be1044c34b3dc9350
kkamochka <k_rinatkyzy@kbtu.kz>
2024-02-24 02:27:45 +0600

    yey

--- week_4/py_math.py
+++ week_4/py_math.py
@@ -1,5 +1,18 @@
 
-print("Input degree: ")
-a = int(input())
+print("Input degree: ", a = int(input())) 
 print("Output radian: ", a*0.0174533 )
 
+class Area():
+    def _init_(self):
+        pass
+class Trapezoid(Area):
+    def _init_(self, height, base1, base2):
+        self.height = height
+        self.base1 = base1
+        self.base2 = base2
+        
+    def solving(self):
+        print( (self.base1 + self.base2)/2 *self.height )
+
+Data = Trapezoid(6, 5, 5)
+Data.solving()
