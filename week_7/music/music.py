9db0cabbcbfea7f68ce9558f4c401b07e722b404
kkamochka <k_rinatkyzy@kbtu.kz>
2024-04-16 01:03:07 +0500

    .

--- week_7/music/music.py
+++ week_7/music/music.py
@@ -1,125 +1,103 @@
-# import pygame
-# import os
-# import sys
-# pygame.init()
-
-# W, H = 850, 850
-
-# display = pygame.display.set_mode([W, H])
-# path = os.getcwd()
-# list = os.listdir(os.path.join(path, "week_7/music")) 
-
-# SONG_END = pygame.USEREVENT + 1
-# pygame.mixer.music.set_endevent(SONG_END)
-
-# # instasamka_zadengida = pygame.mixer.music.load(os.path.join(path, "week_7/music/INSTASAMKA – ЗА ДЕНЬГИ ДА.mp3"))
-# # instasamka_otkltel = pygame.mixer.music.load(os.path.join(path, "week_7/music/INSTASAMKA – Отключаю телефон.mp3"))
-# # doiacat_paintthetownred = pygame.mixer.music.load(os.path.join(path, "week_7/music/Doja Cat – Paint The Town Red.mp3"))
-    
-# def play_next_song():
-#     global songs
-#     songs = songs[1:] + [songs[0]] # move current song to the back of the list
-
-#     pygame.mixer.music.load(songs[0])
-#     pygame.mixer.music.play()
-
-# def play_prev_song():
-#     global songs, songs_len
-#     songs = [songs[-1]] + songs[:songs_len] # get prev song from the end of the list
-
-#     pygame.mixer.music.load(songs[0])
-#     pygame.mixer.music.play()
-
-# # Just get the current song name
-# def get_song_name():
-#     global songs
-#     return songs[0]
-
-
-
-# running = True
-# while running:
-#     for event in pygame.event.get():
-#         if event.type == quit:
-#             pygame.quit()
-#             sys.exit()
-#     pygame.display.update()
-            
-            
-#     buttons = pygame.key.get_pressed()
-#     if buttons[pygame.K_UP]:
-#         pygame.mixer.music.play()
-    
-#     if buttons[pygame.K_DOWN]:
-#         pygame.mixer.music.pause()
-        
-#     if buttons[pygame.K_LEFT]:
-#         pygame.mixer.music.play()
-                
-    
-    
-# pygame.exit()
-
 import pygame
-import os
 import sys
+import os
+
+
+# Path to the songs + list of songs + playlist len
+path_lab = os.path.join(os.getcwd(), "week_7//music//songs")
+songs = [os.path.join("week_7//music//songs", i) for i in os.listdir(path_lab)]
+songs_len = len(songs)
 
 pygame.init()
 
-W, H = 850, 850
-display = pygame.display.set_mode([W, H])
+my_font = pygame.font.SysFont('uuu', 30)
 
-# Получение списка файлов из папки с музыкой
-path = os.getcwd()
-songs = os.listdir(os.path.join(path, "week_7/music"))
-
-# Установка пользовательского события для конца песни
+# Some event
 SONG_END = pygame.USEREVENT + 1
 pygame.mixer.music.set_endevent(SONG_END)
 
-# Индекс текущей играющей песни
-current_song_index = 0
+# The display that we will see
+W, H = 500, 500
+display = pygame.display.set_mode([W, H])
+pygame.display.set_caption("uuu")
 
-# Функция для воспроизведения следующей песни
+# Check paused song or not
+is_paused = False
+
+# Simple methods to play next and prev song
 def play_next_song():
-    global current_song_index, songs
-    current_song_index = (current_song_index + 1) % len(songs) # перемещаем текущую песню в конец списка
-    pygame.mixer.music.load(os.path.join(path, "week_7/music", songs[current_song_index]))
+    global songs
+    songs = songs[1:] + [songs[0]] # move current song to the back of the list
+
+    pygame.mixer.music.load(songs[0])
     pygame.mixer.music.play()
 
-# Функция для воспроизведения предыдущей песни
 def play_prev_song():
-    global current_song_index, songs
-    current_song_index = (current_song_index - 1) % len(songs) # перемещаем текущую песню в начало списка
-    pygame.mixer.music.load(os.path.join(path, "week_7/music", songs[current_song_index]))
+    global songs, songs_len
+    songs = [songs[-1]] + songs[:songs_len] # get prev song from the end of the list
+
+    pygame.mixer.music.load(songs[0])
     pygame.mixer.music.play()
 
-# Основной игровой цикл
-running = True
-while running:
+# Just get the current song name
+def get_song_name():
+    global songs
+    return songs[0]
+
+
+# Main code
+run = True
+while run:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: # Обработка закрытия окна
-            running = False
-        elif event.type == SONG_END: # Обработка конца текущей песни
-            play_next_song()
+        if event.type == SONG_END:
+            play_next_song() 
+        elif event.type == pygame.QUIT:
+            run = False
+        # Pressed once
+        elif event.type == pygame.KEYDOWN:
+            keys = pygame.key.get_pressed() 
 
-    pygame.display.update()
+            # check for left arrow (prev_song)
+            if keys[pygame.K_LEFT]:
+                if is_paused:
+                    pygame.mixer.music.unpause()
+                    is_paused = False
+                play_prev_song()
+            
+            # check for right arrow (next_song)
+            elif keys[pygame.K_RIGHT]:
+                if is_paused:
+                    pygame.mixer.music.unpause()
+                    is_paused = False
+                play_next_song()
+            
+            # check for space (pause song or unpause)
+            elif keys[pygame.K_SPACE]:
+                print(is_paused)
+                if is_paused == False:
+                    pygame.mixer.music.pause()
+                    is_paused = True
+                else:
+                    pygame.mixer.music.unpause()
+                    is_paused = False
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP]: # Воспроизведение текущей песни
-        if not pygame.mixer.music.get_busy():
-            pygame.mixer.music.load(os.path.join(path, "week_7/music", songs[current_song_index]))
-            pygame.mixer.music.play()
+   
+    if not run:
+        break
 
-    if keys[pygame.K_DOWN]: # Пауза/возобновление текущей песни
-        if pygame.mixer.music.get_busy():
-            pygame.mixer.music.pause()
-        else:
-            pygame.mixer.music.unpause()
 
-    if keys[pygame.K_LEFT]: # Воспроизведение предыдущей песни
-        play_prev_song()
+    if not pygame.mixer.music.get_busy() and not is_paused:
+        pygame.mixer.music.load(songs[0])
+        pygame.mixer.music.play()
+    
+    # Text of current playing music
+    text_surface = my_font.render(f'playing: {get_song_name()[16:]}', False, (255, 255, 255))
+    display.fill((0, 0, 0))
+    display.blit(text_surface, (150, 250))
 
-# Завершение Pygame
+    # Update the display
+    pygame.display.flip()
+
+# End of code
 pygame.quit()
 sys.exit()
